3ffa553289c1172d25097c27a1c6fc2928bc3e56
Emircaan <screaps52@hotmail.com>
2023-03-06 17:41:14 +0300

    API Gateway completed. LoggingFilter created

--- currency-convesion-service/src/main/java/com/example/currencyconvesionservice/CurrencyConversionServiceApplication.java
+++ currency-convesion-service/src/main/java/com/example/currencyconvesionservice/CurrencyConversionServiceApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-public class CurrencyConvesionServiceApplication {
+public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CurrencyConvesionServiceApplication.class, args);
+		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
 	}
 
 }
